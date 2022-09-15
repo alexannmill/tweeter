92a25bf4cfff7e18fffe8656f9cb8baf02cb3269
alexannmill <alexannmill@gmail.com>
2022-09-15 06:05:44 +0000

    error handling and xss procetion add

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,57 +3,77 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(function () {
+// const {
+//   createTweetElement,
+//   escape,
+//   $renderTweet,
+//   loadTweets,
+//   } = require(".../tweeter/server/lib/util/data-helpers.js")
 
+$(function () {
   const $tweetContainer = $("#tweet-container");
   const $form = $("#form");
-
+  $("#CharEmpty").hide()
+  $("#CharLimit").hide()
 
   $form.submit((event) => {
     event.preventDefault();
-    const text = $("#tweet-text").val()
+    const text = $("#tweet-text").val();
     //validation of text
+    
     if (text === "" || null) {
-     return alert("Not enough Characters")
+      $("#CharEmpty").slideDown();
+      setTimeout(() => {
+        $("#CharEmpty").slideUp()
+      }, 4000); ;
     }
-    if (text.length > 141){
-      return alert("Over Character Limit")
+    if (text.length > 140) {
+    $("#CharLimit").slideDown();
+    setTimeout(() => {
+      $("#CharLimit").slideUp()
+    }, 4000); ;
     }
+    
     const configData = $form.serialize();
-    $.post("/tweets", configData)
+    $.post("/tweets", configData);
     $.ajax({
       type: "GET",
       url: "/tweets",
-      success: (data) => 
-        loadTweets()
-      })
-    })
+      success: () => loadTweets(),
+    });
+  })
     
   const createTweetElement = (data) => {
     let $tweet = $(`
     <article class="tweet">
-      <header class="tweetheader">
-        <div class="tweetimgname">
-          <img src=${data.user.avatars}>
-          <span class="name">${data.user.name}</span>
+    <header class="tweetheader">
+    <div class="tweetimgname">
+        <img src=${data.user.avatars}>
+        <span class="name">${data.user.name}</span>
         </div>
         <span class="handle">${data.user.handle}</span>
-      </header>
-      <p>
-        ${data.content.text}
+        </header>
+        <p>
+      ${escape(data.content.text)}
       </p>
       <footer class="tweetfooter">
-        <h6 class="timeago">${timeago.format(data.created_at)}</h6>
-        <div class="tweeticons">
-          <i class="fa-sharp fa-solid fa-flag"></i>
-          <i class="fa-sharp fa-solid fa-retweet"></i>
-          <i class="fa-sharp fa-solid fa-heart"></i>
-        </div>
+      <h6 class="timeago">${timeago.format(data.created_at)}</h6>
+      <div class="tweeticons">
+      <i class="fa-sharp fa-solid fa-flag"></i>
+      <i class="fa-sharp fa-solid fa-retweet"></i>
+      <i class="fa-sharp fa-solid fa-heart"></i>
+      </div>
       </footer>
-    </article>
+      </article>
       `);
-    return $tweet;
-  };
+      return $tweet;
+    };
+    
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
       
   const $renderTweet = (data) => {
     $tweetContainer.empty();
@@ -62,16 +82,43 @@ $(function () {
       $tweetContainer.prepend($tweet);
     });
   };
-      
+
   const loadTweets = () => {
     $.ajax({
       type: "GET",
       url: "/tweets",
       success: (data) => {
         $renderTweet(data);
-      }
-    })
+      },
+    });
   };
-  loadTweets()      
+  loadTweets();
 });
- 
+    //     const errCheck = error(text)
+    //     console.log('errCheck:', errCheck)
+    //     const errMessage = errMsg(errCheck)
+    //     console.log('errMessage:', errMessage)
+        
+    //     const errMsg = (errCheck) => {
+    //    const $message = error(text)
+    //    if (!message){
+    //      return
+    //     }
+    //     $(".error-handling").append(`
+    //     <div class=invalid>
+    //     <p>${message}</p>
+    //     </div>
+    //     `)
+    //     $message.slideDown(200);
+    //   }
+      
+    //   const error = (text) => {
+    //   let errorMsg = null
+    //   if (text === "" || null) {
+    //     errorMsg = "Invalid Entry"
+    //   } 
+    //   if (text.length > 141) {
+    //     errorMsg = "Over Character Limit"
+    //   } 
+    //   return errorMsg
+    // }
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -25,3 +25,61 @@ module.exports = function makeDataHelpers(db) {
 
   };
 }
+// const createTweetElement = (data) => {
+//   let $tweet = $(`
+//   <article class="tweet">
+//     <header class="tweetheader">
+//       <div class="tweetimgname">
+//         <img src=${data.user.avatars}>
+//         <span class="name">${data.user.name}</span>
+//       </div>
+//       <span class="handle">${data.user.handle}</span>
+//     </header>
+//     <p>
+//       ${escape(data.content.text)}
+//     </p>
+//     <footer class="tweetfooter">
+//       <h6 class="timeago">${timeago.format(data.created_at)}</h6>
+//       <div class="tweeticons">
+//         <i class="fa-sharp fa-solid fa-flag"></i>
+//         <i class="fa-sharp fa-solid fa-retweet"></i>
+//         <i class="fa-sharp fa-solid fa-heart"></i>
+//       </div>
+//     </footer>
+//   </article>
+//     `);
+//   return $tweet;
+// };
+
+// const escape = function (str) {
+//   let div = document.createElement("div");
+//   div.appendChild(document.createTextNode(str));
+//   return div.innerHTML;
+// };
+
+// const $renderTweet = (data) => {
+//   $tweetContainer.empty();
+//   data.forEach((tweet) => {
+//     const $tweet = createTweetElement(tweet);
+//     $tweetContainer.prepend($tweet);
+//   });
+// };
+
+// const loadTweets = () => {
+//   $.ajax({
+//     type: "GET",
+//     url: "/tweets",
+//     success: (data) => {
+//       $renderTweet(data);
+//     },
+//   });
+// };
+
+
+// module.exports = {
+// createTweetElement,
+// escape,
+// $renderTweet,
+// loadTweets,
+// configData,
+// }
