b6f6e3243a636cdbb4b6d2ff82837e44f8064953
alexannmill <alexannmill@gmail.com>
2022-09-15 01:44:41 +0000

    func for ajax post/get and page render complete, css fixed for proper spacing in tweet boxes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,80 +4,74 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(function () {
+
   const $tweetContainer = $("#tweet-container");
   const $form = $("#form");
 
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
   $form.submit((event) => {
     event.preventDefault();
+    const text = $("#tweet-text").val()
+    //validation of text
+    if (text === "" || null) {
+     return alert("Not enough Characters")
+    }
+    if (text.length > 141){
+      return alert("Over Character Limit")
+    }
+    const configData = $form.serialize();
+    $.post("/tweets", configData)
     $.ajax({
       type: "GET",
       url: "/tweets",
-      success: () => {
-        const configData = $form.serialize();
-        $.post("/tweets", configData);
-        console.log(configData);
-      },
-    });
-  });
-
+      success: (data) => 
+        loadTweets()
+      })
+    })
+    
   const createTweetElement = (data) => {
     let $tweet = $(`
     <article class="tweet">
-        <header class="tweetheader">
-          <div class="tweetimgname">
-            <img src=${data.user.avatars}>
-            <span class="name">${data.user.name}</span>
-          </div>
-          <span class="handle">${data.user.handle}</span>
-        </header>
-        <p>
+      <header class="tweetheader">
+        <div class="tweetimgname">
+          <img src=${data.user.avatars}>
+          <span class="name">${data.user.name}</span>
+        </div>
+        <span class="handle">${data.user.handle}</span>
+      </header>
+      <p>
         ${data.content.text}
-        </p>
-        <footer class="tweetfooter">
-          <h6 class="timestamp">${data.created_at}</h6>
-          <div class="tweeticons">
-            <i class="fa-sharp fa-solid fa-flag"></i>
-            <i class="fa-sharp fa-solid fa-retweet"></i>
-            <i class="fa-sharp fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
-    `);
+      </p>
+      <footer class="tweetfooter">
+        <h6 class="timeago">${timeago.format(data.created_at)}</h6>
+        <div class="tweeticons">
+          <i class="fa-sharp fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-sharp fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+      `);
     return $tweet;
   };
-
+      
   const $renderTweet = (data) => {
     $tweetContainer.empty();
     data.forEach((tweet) => {
-      const $renderTweet = createTweetElement(tweet);
-      $tweetContainer.append($renderTweet);
+      const $tweet = createTweetElement(tweet);
+      $tweetContainer.prepend($tweet);
     });
   };
-  $renderTweet(data);
-
+      
+  const loadTweets = () => {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      success: (data) => {
+        $renderTweet(data);
+      }
+    })
+  };
+  loadTweets()      
 });
+ 
