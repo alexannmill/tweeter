8de1ee9f24b36210129caa44baf60796e8539d88
alexannmill <alexannmill@gmail.com>
2022-09-16 00:30:12 +0000

    sass on style sheets comments for client server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,79 +3,74 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const {
-//   createTweetElement,
-//   escape,
-//   $renderTweet,
-//   loadTweets,
-//   } = require(".../tweeter/server/lib/util/data-helpers.js")
 
 $(function () {
   const $tweetContainer = $("#tweet-container");
   const $form = $("#form");
-  $("#CharEmpty").hide()
-  $("#CharLimit").hide()
-  $(".error-handling").hide()
-
+  $("#CharEmpty").hide();
+  $("#CharLimit").hide();
+  // form submit, prevent refresh
   $form.submit((event) => {
     event.preventDefault();
     const text = $("#tweet-text").val();
+    //conditions and error handling
     if (text === "" || null) {
       $("#CharEmpty").slideDown();
       setTimeout(() => {
-        $("#CharEmpty").slideUp()
-      }, 4000); ;
-      return
+        $("#CharEmpty").slideUp();
+      }, 4000);
+      return;
     }
     if (text.length > 140) {
-    $("#CharLimit").slideDown();
-    setTimeout(() => {
-      $("#CharLimit").slideUp()
-    }, 4000); ;
-    return
+      $("#CharLimit").slideDown();
+      setTimeout(() => {
+        $("#CharLimit").slideUp();
+      }, 4000);
+      return;
     }
-    
+    //config of form data and post
     const configData = $form.serialize();
     $.post("/tweets", configData);
+    //Ajax get req for tweet db after post
     $.ajax({
       type: "GET",
       url: "/tweets",
       success: () => loadTweets(),
     });
-  })
-    
+  });
+  //creating tweet box for new post via appending
   const createTweetElement = (data) => {
     let $tweet = $(`
     <article class="tweet">
     <header class="tweetheader">
-    <div class="tweetimgname">
+      <div class="tweetimgname">
         <img src=${data.user.avatars}>
         <span class="name">${data.user.name}</span>
-        </div>
-        <span class="handle">${data.user.handle}</span>
-        </header>
-        <p>
-      ${escape(data.content.text)}
-      </p>
-      <footer class="tweetfooter">
+      </div>
+      <span class="handle">${data.user.handle}</span>
+    </header>
+    <p>
+    ${escape(data.content.text)}
+    </p>
+    <footer class="tweetfooter">
       <h6 class="timeago">${timeago.format(data.created_at)}</h6>
       <div class="tweeticons">
-      <i class="fa-sharp fa-solid fa-flag"></i>
-      <i class="fa-sharp fa-solid fa-retweet"></i>
-      <i class="fa-sharp fa-solid fa-heart"></i>
+        <i class="fa-sharp fa-solid fa-flag"></i>
+        <i class="fa-sharp fa-solid fa-retweet"></i>
+        <i class="fa-sharp fa-solid fa-heart"></i>
       </div>
-      </footer>
-      </article>
+    </footer>
+    </article>
       `);
-      return $tweet;
-    };
-    
-    const escape = function (str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-      
+    return $tweet;
+  };
+  // protection from xss attacks
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  // render of tweet db and new tweet
   const $renderTweet = (data) => {
     $tweetContainer.empty();
     data.forEach((tweet) => {
@@ -83,7 +78,7 @@ $(function () {
       $tweetContainer.prepend($tweet);
     });
   };
-
+  //Ajax GET for tweet db on page load
   const loadTweets = () => {
     $.ajax({
       type: "GET",
@@ -93,5 +88,6 @@ $(function () {
       },
     });
   };
+
   loadTweets();
 });
